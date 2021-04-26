1d427c853a0fa36f7feafdaeb1cd5b1287180d81
ludiazv <ldiaz@atloideas.com>
2021-04-26 14:24:15 +0200

    add opi lite

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BOARDS=( "opizero" "opione" "opipc" "opipcplus" "bpim2z" "rock64")
+BOARDS=( "opizero" "opione" "opilite" "opipc" "opipcplus" "bpim2z" "rock64")
 echo "==============================="
 echo "Build all tested and WIP boards"
 echo "==============================="
